02c27ee77f585a371e68b98c464628e9a83596e4
Martzz-a <maritzafidelyah@gmail.com>
2026-05-11 15:47:21 +0700

    first commit

---
+++ ibu.h
@@ -0,0 +1,31 @@
+#ifndef IBU_H
+#define IBU_H
+
+#include <iostream>
+#include <vector>
+#include "anak.h"
+using namespace std;
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+    
+};
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (auto &a : daftar_anak) {
+        cout << a->nama << "\n";
+    }
+}
+#endif
